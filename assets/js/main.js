c34011ec9aa53c8635fef7917ebf63f496e8cb80
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-09-01 16:25:39 +0200

    add on click event

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,21 +9,33 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 */
-const fieldEl = document.querySelector('.container');
+const fieldEl = document.querySelector('.container_field');
 
 let limit = 100
 
 
 function griglia(limit) {
+    
     for (let i = 0; i < limit; i++) {
-        console.log('ciao');
-        const markup = `<div class="box">${i + 1}</div>`
-        fieldEl.innerHTML += markup
+        const casellaEL = document.createElement('div')
+        casellaEL.classList.add('box')
+        casellaEL.append(i + 1)
+        fieldEl.append(casellaEL)
 
-
-
-
+        casellaEL.addEventListener('click', function(){
+            casellaEL.classList.toggle('bg_active') 
+            casellaEL.append()
+            console.log(`e stata attivata la casella ${i + 1}`);
+          
+        })
     } 
+    
 }
 
-griglia(limit)
+
+const bottone = document.querySelector('.btn')
+
+bottone.addEventListener('click', function(){
+    griglia(limit)
+
+}, {once : true})
